76a35cfd4ba1c1ba09bf5a9cb06870f9ff7d3381
Ron Le <ronle9519@gmail.com>
2021-06-22 23:21:23 +0700

    add database

--- database/connect.js
+++ database/connect.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const url ="mongodb+srv://rubeha:ronle75@blog.diozp.mongodb.net/test"
 function connect(){
-	mongoose.connect("mongodb+srv://rubeha:<ronle75>@blog.diozp.mongodb.net/Blog?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
-		.then(() => {
-			console.log('Connect to Database successfully!!');
-		})
-		.catch(() => {
-			console.log('Connect failure to Database!!');
-		})
+  try{
+    mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => {
+          console.log('Connect to Database successfully!!');
+        })
+        .catch(() => {
+          console.log('Connect failure to Database!!');
+        })
+  }catch(e){
+    console.log(e);
+  }
+	
 }
 module.exports = {
 	connect
